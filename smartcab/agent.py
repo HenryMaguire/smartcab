0584569df2ff4c18f2b1505421cd908723ebe94a
Henry <henrymaguire@hotmail.co.uk>
2017-10-21 01:13:01 +0100

    implemented reviewer comments.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -38,7 +38,7 @@ class LearningAgent(Agent):
             self.alpha = 0
         else:
             self.epsilon = 0.99**self.time_step
-            #self.epsilon = ()
+            #self.epsilon -= 0.05
             #self.epsilon = (1+self.time_step)**(-2)
             self.time_step += 1
         # Update epsilon using a decay function of your choice
@@ -57,7 +57,7 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)           # Visual input - intersection light and traffic
         deadline = self.env.get_deadline(self)  # Remaining deadline
         # Set 'state' as a tuple of relevant data for the agent
-        state = (waypoint, inputs['light'], inputs['oncoming'], inputs['right'])
+        state = (waypoint, inputs['light'], inputs['oncoming'], inputs['left'])
 
         return state
 
@@ -139,6 +139,49 @@ class LearningAgent(Agent):
 
         return
 
+'''
+def run():
+    """ Driving function for running the simulation.
+        Press ESC to close the simulation, or [SPACE] to pause the simulation. """
+
+    ##############
+    # Create the environment
+    # Flags:
+    #   verbose     - set to True to display additional output from the simulation
+    #   num_dummies - discrete number of dummy agents in the environment, default is 100
+    #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
+    env = Environment(verbose=True)
+
+    ##############
+    # Create the driving agent
+    # Flags:
+    #   learning   - set to True to force the driving agent to use Q-learning
+    #    * epsilon - continuous value for the exploration factor, default is 1
+    #    * alpha   - continuous value for the learning rate, default is 0.5
+    agent = env.create_agent(LearningAgent, learning=True, epsilon=1.)
+
+    ##############
+    # Follow the driving agent
+    # Flags:
+    #   enforce_deadline - set to True to enforce a deadline metric
+    env.set_primary_agent(agent, enforce_deadline=True)
+
+    ##############
+    # Create the simulation
+    # Flags:
+    #   update_delay - continuous time (in seconds) between actions, default is 2.0 seconds
+    #   display      - set to False to disable the GUI if PyGame is enabled
+    #   log_metrics  - set to True to log trial and simulation results to /logs
+    #   optimized    - set to True to change the default log file name
+    sim = Simulator(env, display=False, update_delay=0., log_metrics=True)
+
+    ##############
+    # Run the simulator
+    # Flags:
+    #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
+    #   n_test     - discrete number of testing trials to perform, default is 0
+    sim.run(n_test=10)
+'''
 
 def run():
     """ Driving function for running the simulation.
